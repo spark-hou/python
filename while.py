5791ee99dd3adc939cfe531b1dcc8da3a8b8ae0a
1 <1>
2019-09-16 21:18:33 +0800

    啊哈哈

---
+++ while.py
@@ -0,0 +1,19 @@
+import random
+serect=random.randint(1,10)
+print("answer is :",serect)
+
+
+print("------------spark-text--------------")
+temp=input("enter the number:")
+val=int(temp)
+while val!=serect:
+  
+    if val==serect:
+        print("right")
+
+    else:
+        print("no")
+        temp=input("try again:")
+        val=int(temp)
+
+print("game,over")
