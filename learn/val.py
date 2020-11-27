190e2dc6bfc68bcd659a0e53696ef18c09377a28
侯大大 <1>
2020-11-27 17:30:15 +0800

    two

--- learn/val.py
+++ learn/val.py
@@ -19,3 +19,12 @@ print(arr[:4])
 print(arr * 2)
 
 # Tuple 因为元组是不允许更新的。而列表是允许更新的：
+tuple_test = ('runoob', 786, 2.23, 'john', 70.2)
+
+
+print(tuple_test[1:2])
+print(tuple_test[1:])
+print(tuple_test[:4])
+print(tuple_test * 2)
+
+
